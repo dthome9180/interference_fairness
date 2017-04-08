81247de31259dc18529bf0b367d2876056a92944
dthome9180 <dthome9180@gmail.com>
2017-04-08 13:51:43 +0800

    write FairnessPoint()

--- python/interference.py
+++ python/interference.py
@@ -1,58 +1,71 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# low bandwidth
-x = np.linspace(0.001,1.0,num=1000,endpoint=False)
-
-# max untility for acceptablly worse latency
+# overall bandwidth for queue, peak bandwidth equals 1;
 #u = [0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1]
-u = [0.8]
-"""
-No priority scheduling
-"""
-"""
-for t in u:
-# high bandwidth
-    y = t - x
-#slowdown(x|y)
-    plt.plot(x, 1/(1-(y/(1-x))))
-#slowdown(y|x)
-    plt.plot(x, 1/(1-(x/(1-y))))
-"""
-for t in u:
-    # high bandwidth
-    y = t - x
+BW = 0.8
+
+# bandwidth ratio of latency-sensitive group, 0 < p < 1
+p = np.linspace(0.001,BW/2,num=1000,endpoint=False)
+
+def FairnessPoint(BW, p):
+    # latency-sensitive group badwidth and performance alone
+    LowBW = BW * p
+    Perf_LowBW_alone = 1.0/(1-LowBW)
+    # bandwidth intensive group bandwidth and performance alone
+    HighBW = BW - LowBW
+    Perf_HighBW_alone = 1.0/(1-HighBW)
+
     # high priority ratio in x (latency sensitive)
-    hpr_x = ((1/x)/((1/x)+(1/y)))
-    # high priority bandwidth
-    #r1 = x * hpr_x + y * (1-hpr_x) #this priority scheduling maybe not feasible
-                                    #becasue the order between two queue need 
-                                    #considered
-    r1 = x * hpr_x
-    #low priority bandwidth: y + low proiority bandwidth of x
-    #r2 = x * (1-hpr_x) + y * hpr_x #not feasible see corresponding comments
-    r2 = x * (1-hpr_x) + y
-    # high priority latency
-    l_hp=(1+r2)/(1-r1)
-    # low priority latency
-    l_lp=(1-r1*(1-r1-r2))/((1-r1)*(1-r1-r2))
-    
-    perf_x_alone=1-x
-    perf_y_alone=1-y
-    #shared performance with priority scheduling
-    perf_x_share_p=1.0/(l_hp*hpr_x+l_lp*(1-hpr_x))
-    perf_y_share_p=1.0/l_lp
-    #shared performane without priority scheduling
-    perf_share=1.0/(1.0/(1-t))
-    
-    slowdown_x_y=perf_x_alone/perf_x_share_p
-    slowdown_y_x=perf_y_alone/perf_y_share_p
-    #shared whith priority slowdwon to alone
-    plt.plot(x, slowdown_x_y, 'red')
-    plt.plot(x, slowdown_y_x, 'blue')
-    #shared with priority slowdown to shared without priority
-    plt.plot(x, perf_share/perf_x_share_p, 'black')
-    plt.plot(x, perf_share/perf_y_share_p, 'green')
+    hpr = np.linspace(0,1.0,num=1000,endpoint=True)
+
+    SlowdownPair = {}
+
+    # assign h * LowBW high priority, others low priority
+    for h in hpr:
+        # high priority bandwidth
+        HighPriBW = LowBW * h
+        #low priority bandwidth
+        LowPriBW = LowBW * (1-h) + HighBW
+
+        HighPriRatio = HighPriBW/1
+        LowPriRatio = LowPriBW/1
+
+        # high priority Average Response Time (ART)
+        ART_HighPriBW = (1+LowPriRatio)/(1-HighPriRatio)/1
+        # low priority Average Response Time (ART)
+        ART_LowPriBW = (1-HighPriRatio*(1-HighPriRatio-LowPriRatio))/\
+                        ((1-HighPriRatio)*(1-HighPriRatio-LowPriRatio))/1
+
+        #shared performance with priority scheduling
+        ART_LowBW = ART_HighPriBW*h + ART_LowPriBW*(1-h)
+        ART_HighBW = ART_LowPriBW
+
+        Perf_LowBW = 1.0/ART_LowBW
+        Perf_HighBW = 1.0/ART_HighBW
+
+        Slowdown_LowBW = Perf_LowBW_alone/Perf_LowBW
+        Slowdown_HighBW = Perf_HighBW_alone/Perf_HighBW
+
+        #shared whith priority slowdwon to alone
+        plt.plot(h, Slowdown_LowBW, 'red')
+        plt.plot(h, Slowdown_HighBW, 'blue')
+
+        SlowdownPair[h] = [Slowdown_LowBW, Slowdown_HighBW]
+
+    #SlowdownFairDiff = abs(SlowdownPair[0][0]-SlowdownPair[0][1])
+    #SlowdownFairPoint = 0
+    return #SlowdownFairPoint
+
+"""
+    for t in hpr:
+        if abs(SlowdownPair[t][0]-SlowdownPair[t][1])<SlowdownFairDiff:
+            SlowdownFairDiff = abs(t[0][0]-t[0][1])
+            SlowdownFairPoint = t
+"""
+
+
+FairnessPoint(BW,p)
+print x
 
 plt.show()
-
